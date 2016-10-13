8e41257f89f81165b803198ca1abefce5e1715c4
Matvivone <matiasvivone@hotmail.com>
2016-10-13 01:25:57 -0300

    Interface
    Los controles se activan o desactivan dependiendo lo que el usuario
    quiere hacer (crear nuevo o buscar)

--- ClinicaFrba/ClinicaFrba/Abm Afiliado/frm_administrar_afiliados.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/frm_administrar_afiliados.Designer.cs
@@ -54,14 +54,17 @@
             this.Nuevo = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
             this.tex_dni_asociado_principal = new System.Windows.Forms.TextBox();
-            this.opcional = new System.Windows.Forms.Label();
             this.dtp_fecha_nacimiento = new System.Windows.Forms.DateTimePicker();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.but_cargarnuevo = new System.Windows.Forms.Button();
+            this.but_descartarNuevo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Location = new System.Drawing.Point(119, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 0;
@@ -70,9 +73,12 @@
             // 
             // but_buscar
             // 
-            this.but_buscar.Location = new System.Drawing.Point(205, 13);
+            this.but_buscar.FlatAppearance.BorderSize = 3;
+            this.but_buscar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.but_buscar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.but_buscar.Location = new System.Drawing.Point(15, 46);
             this.but_buscar.Name = "but_buscar";
-            this.but_buscar.Size = new System.Drawing.Size(51, 24);
+            this.but_buscar.Size = new System.Drawing.Size(96, 29);
             this.but_buscar.TabIndex = 1;
             this.but_buscar.Text = "Buscar";
             this.but_buscar.UseVisualStyleBackColor = true;
@@ -80,33 +86,33 @@
             // 
             // but_modificar
             // 
-            this.but_modificar.Location = new System.Drawing.Point(136, 332);
+            this.but_modificar.Location = new System.Drawing.Point(29, 376);
             this.but_modificar.Name = "but_modificar";
-            this.but_modificar.Size = new System.Drawing.Size(83, 24);
+            this.but_modificar.Size = new System.Drawing.Size(117, 48);
             this.but_modificar.TabIndex = 3;
             this.but_modificar.Text = "Modificar";
             this.but_modificar.UseVisualStyleBackColor = true;
             // 
             // but_eliminar
             // 
-            this.but_eliminar.Location = new System.Drawing.Point(251, 332);
+            this.but_eliminar.Location = new System.Drawing.Point(186, 376);
             this.but_eliminar.Name = "but_eliminar";
-            this.but_eliminar.Size = new System.Drawing.Size(83, 24);
+            this.but_eliminar.Size = new System.Drawing.Size(131, 48);
             this.but_eliminar.TabIndex = 4;
             this.but_eliminar.Text = "Eliminar";
             this.but_eliminar.UseVisualStyleBackColor = true;
             // 
             // tex_numero_afiliado
             // 
-            this.tex_numero_afiliado.Location = new System.Drawing.Point(113, 16);
+            this.tex_numero_afiliado.Location = new System.Drawing.Point(234, 51);
             this.tex_numero_afiliado.Name = "tex_numero_afiliado";
-            this.tex_numero_afiliado.Size = new System.Drawing.Size(86, 20);
+            this.tex_numero_afiliado.Size = new System.Drawing.Size(114, 20);
             this.tex_numero_afiliado.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Location = new System.Drawing.Point(12, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 6;
@@ -116,7 +122,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Location = new System.Drawing.Point(12, 133);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 7;
@@ -125,7 +131,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 116);
+            this.label4.Location = new System.Drawing.Point(12, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 8;
@@ -134,7 +140,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Location = new System.Drawing.Point(12, 184);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(109, 13);
             this.label5.TabIndex = 9;
@@ -143,7 +149,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 168);
+            this.label6.Location = new System.Drawing.Point(12, 212);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(55, 13);
             this.label6.TabIndex = 10;
@@ -153,7 +159,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(9, 190);
+            this.label7.Location = new System.Drawing.Point(9, 234);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(52, 13);
             this.label7.TabIndex = 11;
@@ -162,7 +168,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 215);
+            this.label8.Location = new System.Drawing.Point(12, 259);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(35, 13);
             this.label8.TabIndex = 12;
@@ -170,50 +176,50 @@
             // 
             // tex_nombre
             // 
-            this.tex_nombre.Location = new System.Drawing.Point(117, 62);
+            this.tex_nombre.Location = new System.Drawing.Point(145, 103);
             this.tex_nombre.Name = "tex_nombre";
             this.tex_nombre.Size = new System.Drawing.Size(204, 20);
             this.tex_nombre.TabIndex = 13;
             // 
             // tex_apellido
             // 
-            this.tex_apellido.Location = new System.Drawing.Point(117, 88);
+            this.tex_apellido.Location = new System.Drawing.Point(145, 129);
             this.tex_apellido.Name = "tex_apellido";
             this.tex_apellido.Size = new System.Drawing.Size(204, 20);
             this.tex_apellido.TabIndex = 14;
             // 
             // tex_dni
             // 
-            this.tex_dni.Location = new System.Drawing.Point(117, 114);
+            this.tex_dni.Location = new System.Drawing.Point(145, 154);
             this.tex_dni.Name = "tex_dni";
-            this.tex_dni.Size = new System.Drawing.Size(121, 20);
+            this.tex_dni.Size = new System.Drawing.Size(204, 20);
             this.tex_dni.TabIndex = 15;
             // 
             // tex_direccion
             // 
-            this.tex_direccion.Location = new System.Drawing.Point(117, 165);
+            this.tex_direccion.Location = new System.Drawing.Point(145, 207);
             this.tex_direccion.Name = "tex_direccion";
-            this.tex_direccion.Size = new System.Drawing.Size(86, 20);
+            this.tex_direccion.Size = new System.Drawing.Size(204, 20);
             this.tex_direccion.TabIndex = 17;
             // 
             // tex_telefono
             // 
-            this.tex_telefono.Location = new System.Drawing.Point(117, 189);
+            this.tex_telefono.Location = new System.Drawing.Point(145, 231);
             this.tex_telefono.Name = "tex_telefono";
-            this.tex_telefono.Size = new System.Drawing.Size(86, 20);
+            this.tex_telefono.Size = new System.Drawing.Size(204, 20);
             this.tex_telefono.TabIndex = 18;
             // 
             // tex_mail
             // 
-            this.tex_mail.Location = new System.Drawing.Point(117, 215);
+            this.tex_mail.Location = new System.Drawing.Point(145, 257);
             this.tex_mail.Name = "tex_mail";
-            this.tex_mail.Size = new System.Drawing.Size(86, 20);
+            this.tex_mail.Size = new System.Drawing.Size(204, 20);
             this.tex_mail.TabIndex = 19;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 244);
+            this.label9.Location = new System.Drawing.Point(12, 288);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(34, 13);
             this.label9.TabIndex = 20;
@@ -222,7 +228,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(12, 269);
+            this.label10.Location = new System.Drawing.Point(12, 313);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(100, 13);
             this.label10.TabIndex = 21;
@@ -231,46 +237,49 @@
             // rdb_masculino
             // 
             this.rdb_masculino.AutoSize = true;
-            this.rdb_masculino.Location = new System.Drawing.Point(113, 242);
+            this.rdb_masculino.Location = new System.Drawing.Point(186, 283);
             this.rdb_masculino.Name = "rdb_masculino";
             this.rdb_masculino.Size = new System.Drawing.Size(73, 17);
             this.rdb_masculino.TabIndex = 22;
             this.rdb_masculino.TabStop = true;
             this.rdb_masculino.Text = "Masculino";
             this.rdb_masculino.UseVisualStyleBackColor = true;
+            this.rdb_masculino.CheckedChanged += new System.EventHandler(this.rdb_masculino_CheckedChanged);
             // 
             // rdb_femenino
             // 
             this.rdb_femenino.AutoSize = true;
-            this.rdb_femenino.Location = new System.Drawing.Point(205, 242);
+            this.rdb_femenino.Location = new System.Drawing.Point(278, 283);
             this.rdb_femenino.Name = "rdb_femenino";
             this.rdb_femenino.Size = new System.Drawing.Size(71, 17);
             this.rdb_femenino.TabIndex = 23;
             this.rdb_femenino.TabStop = true;
             this.rdb_femenino.Text = "Femenino";
             this.rdb_femenino.UseVisualStyleBackColor = true;
+            this.rdb_femenino.CheckedChanged += new System.EventHandler(this.rdb_femenino_CheckedChanged);
             // 
             // com_plan_medico
             // 
             this.com_plan_medico.FormattingEnabled = true;
-            this.com_plan_medico.Location = new System.Drawing.Point(117, 266);
+            this.com_plan_medico.Location = new System.Drawing.Point(145, 307);
             this.com_plan_medico.Name = "com_plan_medico";
-            this.com_plan_medico.Size = new System.Drawing.Size(121, 21);
+            this.com_plan_medico.Size = new System.Drawing.Size(204, 21);
             this.com_plan_medico.TabIndex = 24;
             // 
             // Nuevo
             // 
-            this.Nuevo.Location = new System.Drawing.Point(15, 332);
+            this.Nuevo.Location = new System.Drawing.Point(14, 12);
             this.Nuevo.Name = "Nuevo";
-            this.Nuevo.Size = new System.Drawing.Size(61, 24);
+            this.Nuevo.Size = new System.Drawing.Size(97, 28);
             this.Nuevo.TabIndex = 25;
             this.Nuevo.Text = "Nuevo";
             this.Nuevo.UseVisualStyleBackColor = true;
+            this.Nuevo.Click += new System.EventHandler(this.Nuevo_Click);
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(12, 297);
+            this.label11.Location = new System.Drawing.Point(12, 341);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(134, 13);
             this.label11.TabIndex = 26;
@@ -278,35 +287,66 @@
             // 
             // tex_dni_asociado_principal
             // 
-            this.tex_dni_asociado_principal.Location = new System.Drawing.Point(147, 293);
+            this.tex_dni_asociado_principal.Location = new System.Drawing.Point(145, 334);
             this.tex_dni_asociado_principal.Name = "tex_dni_asociado_principal";
-            this.tex_dni_asociado_principal.Size = new System.Drawing.Size(121, 20);
+            this.tex_dni_asociado_principal.Size = new System.Drawing.Size(204, 20);
             this.tex_dni_asociado_principal.TabIndex = 27;
-            // 
-            // opcional
-            // 
-            this.opcional.AutoSize = true;
-            this.opcional.Location = new System.Drawing.Point(274, 300);
-            this.opcional.Name = "opcional";
-            this.opcional.Size = new System.Drawing.Size(53, 13);
-            this.opcional.TabIndex = 28;
-            this.opcional.Text = "(opcional)";
-            this.opcional.Click += new System.EventHandler(this.opcional_Click);
             // 
             // dtp_fecha_nacimiento
             // 
-            this.dtp_fecha_nacimiento.Location = new System.Drawing.Point(128, 140);
+            this.dtp_fecha_nacimiento.Location = new System.Drawing.Point(145, 181);
             this.dtp_fecha_nacimiento.Name = "dtp_fecha_nacimiento";
-            this.dtp_fecha_nacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtp_fecha_nacimiento.Size = new System.Drawing.Size(204, 20);
             this.dtp_fecha_nacimiento.TabIndex = 29;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(374, 436);
+            this.shapeContainer1.TabIndex = 30;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 15;
+            this.lineShape1.X2 = 347;
+            this.lineShape1.Y1 = 89;
+            this.lineShape1.Y2 = 89;
+            // 
+            // but_cargarnuevo
+            // 
+            this.but_cargarnuevo.Location = new System.Drawing.Point(38, 376);
+            this.but_cargarnuevo.Name = "but_cargarnuevo";
+            this.but_cargarnuevo.Size = new System.Drawing.Size(124, 48);
+            this.but_cargarnuevo.TabIndex = 31;
+            this.but_cargarnuevo.Text = "Cargar nuevo afiliado";
+            this.but_cargarnuevo.UseVisualStyleBackColor = true;
+            this.but_cargarnuevo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // but_descartarNuevo
+            // 
+            this.but_descartarNuevo.Location = new System.Drawing.Point(198, 376);
+            this.but_descartarNuevo.Name = "but_descartarNuevo";
+            this.but_descartarNuevo.Size = new System.Drawing.Size(131, 48);
+            this.but_descartarNuevo.TabIndex = 32;
+            this.but_descartarNuevo.Text = "Descartar";
+            this.but_descartarNuevo.UseVisualStyleBackColor = true;
+            this.but_descartarNuevo.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // frm_administrar_afiliados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 394);
+            this.ClientSize = new System.Drawing.Size(374, 436);
+            this.Controls.Add(this.but_descartarNuevo);
+            this.Controls.Add(this.but_cargarnuevo);
             this.Controls.Add(this.dtp_fecha_nacimiento);
-            this.Controls.Add(this.opcional);
             this.Controls.Add(this.tex_dni_asociado_principal);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.Nuevo);
@@ -333,6 +373,7 @@
             this.Controls.Add(this.but_modificar);
             this.Controls.Add(this.but_buscar);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
             this.Name = "frm_administrar_afiliados";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Administrar Afiliados";
@@ -370,7 +411,10 @@
         private System.Windows.Forms.Button Nuevo;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox tex_dni_asociado_principal;
-        private System.Windows.Forms.Label opcional;
         private System.Windows.Forms.DateTimePicker dtp_fecha_nacimiento;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button but_cargarnuevo;
+        private System.Windows.Forms.Button but_descartarNuevo;
     }
 }
--- ClinicaFrba/ClinicaFrba/Abm Afiliado/frm_administrar_afiliados.cs
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/frm_administrar_afiliados.cs
@@ -19,7 +19,21 @@ namespace ClinicaFrba.AbmRol
 
         private void frm_administrar_afiliados_Load(object sender, EventArgs e)
         {
-
+            tex_nombre.Enabled = false;
+            tex_apellido.Enabled = false;
+            tex_dni.Enabled = false;
+            dtp_fecha_nacimiento.Enabled = false;
+            tex_direccion.Enabled = false;
+            tex_telefono.Enabled = false;
+            tex_mail.Enabled = false;
+            rdb_femenino.Enabled = false;
+            rdb_masculino.Enabled = false;
+            com_plan_medico.Enabled = false;
+            tex_dni_asociado_principal.Enabled = false;
+            but_modificar.Visible = false;
+            but_eliminar.Visible = false;
+            but_cargarnuevo.Visible = false;
+            but_descartarNuevo.Visible = false;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -48,7 +62,23 @@ namespace ClinicaFrba.AbmRol
         }
 
         private void but_buscar_Click(object sender, EventArgs e)
-        {
+        {   
+            //Habilito controles
+            tex_numero_afiliado.Enabled = true;
+            tex_nombre.Enabled = true;
+            tex_apellido.Enabled = true;
+            tex_dni.Enabled = true;
+            dtp_fecha_nacimiento.Enabled = true;
+            tex_direccion.Enabled = true;
+            tex_telefono.Enabled = true;
+            tex_mail.Enabled = true;
+            rdb_femenino.Enabled = true;
+            rdb_masculino.Enabled = true;
+            com_plan_medico.Enabled = true;
+            tex_dni_asociado_principal.Enabled = true;
+            but_modificar.Visible = true;
+            but_eliminar.Visible = true;
+
             //Conecto a BD
             Conexion conexion = new Conexion();
             conexion.conectar();
@@ -66,6 +96,61 @@ namespace ClinicaFrba.AbmRol
             tex_mail.Text = paciente.GetValue(5).ToString();
             tex_dni_asociado_principal.Text = paciente.GetValue(6).ToString();
             //tex_fecha.Text = paciente.GetValue(3).ToString();
+        }
+
+        private void rdb_femenino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdb_masculino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nuevo_Click(object sender, EventArgs e)
+        {
+            but_buscar.Enabled = false;
+            tex_numero_afiliado.Text = "";
+            tex_nombre.Text = "";
+            tex_apellido.Text = "";
+            tex_dni.Text = "";
+            tex_telefono.Text = "";
+            tex_mail.Text = "";
+            tex_dni_asociado_principal.Text = "";
+            tex_direccion.Text = "";
+
+            tex_numero_afiliado.Enabled = false;
+            but_modificar.Visible = false;
+            but_eliminar.Visible = false;
+            but_cargarnuevo.Visible = true;
+            but_descartarNuevo.Visible = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            tex_numero_afiliado.Enabled = true;
+            tex_nombre.Enabled = false;
+            tex_apellido.Enabled = false;
+            tex_dni.Enabled = false;
+            dtp_fecha_nacimiento.Enabled = false;
+            tex_direccion.Enabled = false;
+            tex_telefono.Enabled = false;
+            tex_mail.Enabled = false;
+            rdb_femenino.Enabled = false;
+            rdb_masculino.Enabled = false;
+            com_plan_medico.Enabled = false;
+            tex_dni_asociado_principal.Enabled = false;
+            but_modificar.Visible = false;
+            but_eliminar.Visible = false;
+            but_cargarnuevo.Visible = false;
+            but_descartarNuevo.Visible = false;
+            but_buscar.Enabled = true;
         }
     }
 }
